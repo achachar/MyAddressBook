3c3927f839405842eed334ff41f3fda29d5d124e
Anees Chachar <achachar@corp.contoursoftware.net>
2017-12-21 00:39:25 +0500

    changes made after raees review

--- AddressBook/urls.py
+++ AddressBook/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('main_app.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
+    ]
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,7 @@
 __author__ = 'achachar'
 from django.conf.urls import url
 from . import views
-from django.views.static import serve
-from django.conf import settings
+
 
 app_name = 'main_app'
 urlpatterns = [
@@ -10,8 +9,3 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^add_contact/?$', views.add_contact, name='add_contact')
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
-    ]
