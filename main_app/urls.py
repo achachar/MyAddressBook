70847c06da74b41828406a2e6263a3d7424d3cca
Anees Chachar <achachar@corp.contoursoftware.net>
2017-12-21 00:17:28 +0500

    added image field and incomplete login functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
 __author__ = 'achachar'
 from django.conf.urls import url
 from . import views
+from django.views.static import serve
+from django.conf import settings
 
 app_name = 'main_app'
 urlpatterns = [
@@ -8,3 +10,8 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^add_contact/?$', views.add_contact, name='add_contact')
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
+    ]
